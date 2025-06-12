7ffa145f358e015696bfb92453ec910a116a1c8b
Somnath Das <writeatsomnath@gmail.com>
2025-06-12 18:15:32 +0530

    update CICD code

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (replace per region)
+  ami           = "ami-09e6f87a47903347c"  # Amazon Linux 2 AMI (replace per region)
   instance_type = "t2.micro"
 
   tags = {
